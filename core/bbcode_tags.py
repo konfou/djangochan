e2381e0082eec0fecd3d659e3699333f1fd6573b
Konstantinos Foutzopoulos <mail@konfou.xyz>
2023-01-04 11:52:36 +0200

    add roll and spoiler bbtag

---
+++ core/bbcode_tags.py
@@ -0,0 +1,20 @@
+from precise_bbcode.bbcode.tag import BBCodeTag
+from precise_bbcode.tag_pool import tag_pool
+
+from random import randint
+
+class SpoilerTag(BBCodeTag):
+    name = 'spoiler'
+    definition_string = '[spoiler]{TEXT}[/spoiler]'
+    format_string = '<span class="spoiler">{TEXT}</span>'
+
+class RollTag(BBCodeTag):
+    name = 'roll'
+    definition_string = '[roll]'
+    format_string = '<span class="roll">Roll: '  + str(randint(1,100)) + '</span>'
+
+    class Options:
+        standalone = True
+
+tag_pool.register_tag(SpoilerTag)
+tag_pool.register_tag(RollTag)
