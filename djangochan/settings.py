d4e097db3e8490f08b8783ea5792485eddc1ecf8
Konstantinos Foutzopoulos <mail@konfou.xyz>
2023-01-05 00:48:50 +0200

    refactor templates html with modifiable site title

--- djangochan/settings.py
+++ djangochan/settings.py
@@ -16,6 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SITE_TITLE = "djangochan"
+SITE_DESCRIPTION = "after the showers the sun will be shining"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
