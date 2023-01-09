a0ffa2b56e0c175cff449b1ff8d10ccae2edd52d
Konstantinos Foutzopoulos <mail@konfou.xyz>
2023-01-09 13:17:40 +0200

    minor changes

--- core/forms.py
+++ core/forms.py
@@ -7,11 +7,14 @@ from .models import Post
 
 # TODO: simplify this
 
+
 class NewThreadForm(forms.ModelForm):
     if settings.CAPTCHA:
-        captcha = MathCaptchaField(widget=MathCaptchaWidget(question_tmpl="%(num1)i %(operator)s %(num2)i = "))
+        captcha = MathCaptchaField(widget=MathCaptchaWidget(
+            question_tmpl="%(num1)i %(operator)s %(num2)i = "))
 
-    options = forms.CharField(label='Options', required=False, empty_value=None)
+    options = forms.CharField(
+        label='Options', required=False, empty_value=None)
 
     class Meta:
         model = Post
@@ -21,11 +24,14 @@ class NewThreadForm(forms.ModelForm):
 
     field_order = ['author', 'options']
 
+
 class NewReplyForm(forms.ModelForm):
     if settings.CAPTCHA:
-        captcha = MathCaptchaField(widget=MathCaptchaWidget(question_tmpl="%(num1)i %(operator)s %(num2)i = "))
+        captcha = MathCaptchaField(widget=MathCaptchaWidget(
+            question_tmpl="%(num1)i %(operator)s %(num2)i = "))
 
-    options = forms.CharField(label='Options', required=False, empty_value=None)
+    options = forms.CharField(
+        label='Options', required=False, empty_value=None)
 
     class Meta:
         model = Post
