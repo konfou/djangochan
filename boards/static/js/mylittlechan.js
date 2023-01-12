c226841f9485f2c287a91e041fb4d20d043aa1ba
Konstantinos Foutzopoulos <mail@konfou.xyz>
2023-01-12 05:24:56 +0200

    client-side dynamicism with htmx

--- boards/static/js/mylittlechan.js
+++ boards/static/js/mylittlechan.js
@@ -1,11 +1,19 @@
-const images = document.querySelectorAll('.image');
-const viewimage = document.querySelector('#viewimage');
+function onbodyload() {
+    try { // at board or thread
+        buttonSubmit = document.querySelector('input[type=submit]').remove();
+        document.getElementsByClassName('text-submit')[0].style.visibility = "visible";
+    } catch (e) {} // at index
+    var viewimage = document.querySelector('#viewimage');
+    var images = document.querySelectorAll('.image');
+    images.forEach((span) => {
+        a = span.firstChild
+        a.addEventListener('click', (e) => {
+            viewimage.style.display = 'block';
+            viewimage.style.backgroundImage = 'url(' + e.currentTarget.href + ')';
+            e.preventDefault();
+        }, false);
+    });
+}
 
-images.forEach((span) => {
-    a = span.firstChild
-    a.addEventListener('click', (e) => {
-        viewimage.style.display = 'block';
-        viewimage.style.backgroundImage = 'url(' + a.href + ')';
-        e.preventDefault();
-    }, false);
-});
+onbodyload()
+htmx.on('htmx:afterOnLoad', onbodyload)
